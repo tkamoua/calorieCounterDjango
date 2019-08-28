955d4e904d4022254f3136747d5aa9920c170eaa
tkamoua <tkamoua@umich.edu>
2019-08-28 15:11:57 -0400

    fixed delete button and added css style

--- myproject/michFit/views.py
+++ myproject/michFit/views.py
@@ -8,19 +8,13 @@ cart = []
 totalCalories = 0
 totalProtein = 0
 def home(request):
-    menu = MenuItem.objects.order_by('name')[:]
+    menu = MenuItem.objects.all()
     context = {'menu': menu}
     MenuItem.objects.all().delete()
     for key, value in calorieCounter.itemsDict.items():
         MenuItem.objects.create_item(key, value[0],value[1])
     return render(request,'michFit/index.html',context)
 
-def detail(request,name):
-    try:
-        item=MenuItem.objects.get(pk=name)
-    except MenuItem.DoesNotExist:
-        raise Http404("Item does not exist")
-    return render(request, 'michFit/detail.html',{'item': item})
 
 def cartdelete(request, item_id):
     cart.remove(MenuItem.objects.get(pk=item_id))
@@ -29,19 +23,19 @@ def cartdelete(request, item_id):
     for item in cart:
         totalCalories += item.calories
         totalProtein+=item.protein
-    return render(request,'michFit/cart.html',{'cart': cart, 'totalCalories': totalCalories, 'totalProtein': totalProtein} )
-
-def cart_add(request, item_id):
-    cart.append(MenuItem.objects.get(pk=item_id))
-    totalCalories = 0
-    totalProtein=0
-    for item in cart:
-        totalCalories += item.calories
-        totalProtein+=item.protein
-    
     return render(request, 'michFit/cart.html',{'cart': cart, 'totalCalories': totalCalories, 'totalProtein': totalProtein} )
+
+def cart_add( request,item_id):
+    cart.append(MenuItem.objects.get(pk=item_id))
+    menu = MenuItem.objects.all()
+    context = {'menu': menu}
+    return render(request,'michFit/index.html',context)
+    
+    
+   
     
 def cart_view(request,item_id):
+
     totalCalories = 0
     totalProtein=0
     for item in cart:
